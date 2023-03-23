8fc35a38bf94833ded5c057fe0b8bb93f9d23592
evan-carlsen <evan@crowdbotics.com>
2023-03-23 15:34:58 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "dev-onb-demo-101-39529",
-  url: "https://dev-onb-demo-101-39529.botics.co",
-  api: "https://dev-onb-demo-101-39529.botics.co/api/v1"
-}
+  "name": "dev-onb-demo-101-39529",
+  "url": "https://dev-onb-demo-101-39529.botics.co",
+  "api": "https://dev-onb-demo-101-39529.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
